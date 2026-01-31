755eb4738c57ea27f3eeb7ab1fe06226621f3277
Priya172385 <ammup8043@gmail.com>
2026-01-31 11:46:35 +0530

    Replace water tracker with recipe keeper functionality

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
+st.title("🍲 Home Recipe Keeper")
+if "recipes" not in st.session_state:
+    st.session_state.recipes = []
 
-st.title("💧 Water Intake Tracker")
-if "water" not in st.session_state:
-    st.session_state.water = 0
+recipe_name = st.text_input("Recipe Name")
+ingredients = st.text_area("Ingredients (comma separated)")
 
-water_glass = st.number_input("Add glasses of water", 1, 10, 1)
-if st.button("Add"):
-    st.session_state.water += water_glass
+if st.button("Add Recipe"):
+    if recipe_name and ingredients:
+        st.session_state.recipes.append({"Recipe": recipe_name, "Ingredients": ingredients})
 
-st.subheader("Today's Water Intake")
-st.write(f"You drank {st.session_state.water} glass(es) of water today! 🚰")
+st.subheader("Your Recipes")
+for r in st.session_state.recipes:
+    st.write(f"• {r['Recipe']}: {r['Ingredients']}")
+
 
 
 
