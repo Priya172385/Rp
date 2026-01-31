030a751db6918fd4ac96a001d27b8498e646e58b
Priya172385 <ammup8043@gmail.com>
2026-01-31 11:45:47 +0530

    Change app from chores tracker to water intake tracker

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 
-st.title("🏠 Home Chores Tracker")
-if "chores" not in st.session_state:
-    st.session_state.chores = []
+st.title("💧 Water Intake Tracker")
+if "water" not in st.session_state:
+    st.session_state.water = 0
 
-chore = st.text_input("Chore Name")
-if st.button("Add Chore"):
-    if chore:
-        st.session_state.chores.append(chore)
+water_glass = st.number_input("Add glasses of water", 1, 10, 1)
+if st.button("Add"):
+    st.session_state.water += water_glass
 
-st.subheader("Chore List")
-for i, c in enumerate(st.session_state.chores, 1):
-    st.write(f"{i}. {c}")
+st.subheader("Today's Water Intake")
+st.write(f"You drank {st.session_state.water} glass(es) of water today! 🚰")
+
 
 
 
